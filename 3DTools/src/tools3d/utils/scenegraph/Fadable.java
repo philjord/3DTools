91ef755cd69cf4de87fb483a7721d39cff1d6acc
phil <phil@Thinky>
2015-10-17 00:29:11 +0200

    new outline method added to fadable interface(possibly not a good name now)

--- 3DTools/src/tools3d/utils/scenegraph/Fadable.java
+++ 3DTools/src/tools3d/utils/scenegraph/Fadable.java
@@ -1,9 +1,19 @@
 package tools3d.utils.scenegraph;
 
+import javax.vecmath.Color3f;
+
 public interface Fadable
 {
 	/**percent is from 0 to 1.0 and will be clamped if over/under
 	* set 0 to use normal transparency for shape
 	*/
 	public void fade(float percent);
+
+	/** 
+	 * handy place to put this
+	 * use null to indicate disable outline
+	 * 
+	 * @param c
+	 */
+	public void setOutline(Color3f c);
 }
